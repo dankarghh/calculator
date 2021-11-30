df427822041304bdfa7c12e4c9bab6e3d6aac70d
dankarghh <93171130+dankarghh@users.noreply.github.com>
2021-11-30 14:47:11 +1000

    added broken functionality

--- index.js
+++ index.js
@@ -1,8 +1,77 @@
-const message = document.querySelector(".calculator_screen");
-const btn7 = document.getElementById("btn7");
+const lastOperationScreen = document.querySelector(".screen-last");
+const currentOperationScreen = document.querySelector(".screen-current");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationsButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const deleteButton = document.querySelector("[data-delete");
 
-btn7.addEventListener("click", (e) => {
-  message.textContent = 7;
+let firstOperand = "";
+let secondOperand = "";
+let operator = undefined;
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let value = e.target.innerText;
+    currentOperationScreen.textContent += value;
+  });
 });
 
-console.log(btn7);
+operationsButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    operator = e.target.innerText;
+    if (firstOperand === "") {
+      firstOperand = Number(currentOperationScreen.innerText);
+      lastOperationScreen.textContent = `${currentOperationScreen.innerText} ${operator}`;
+      currentOperationScreen.textContent = "";
+    } else {
+      lastOperationScreen.textContent = `${currentOperationScreen.innerText} ${operator}`;
+      firstOperand = Number(currentOperationScreen.innerText);
+      currentOperationScreen.textContent = "";
+    }
+  });
+});
+
+clearButton.addEventListener("click", (e) => {
+  reset();
+});
+
+equalsButton.addEventListener("click", (e) => {
+  secondOperand = Number(currentOperationScreen.innerText);
+  console.log(operator);
+  let result = add(firstOperand, secondOperand);
+
+  lastOperationScreen.textContent = `${lastOperationScreen.innerText} ${currentOperationScreen.innerText}`;
+  currentOperationScreen.innerText = `${result}`;
+});
+
+//BASIC FUNCTIONALITY
+
+function add(firstOperand, secondOperand) {
+  return firstOperand + secondOperand;
+}
+function subtract(firstOperand, secondOperand) {
+  return firstOperand - secondOperand;
+}
+function multiply(firstOperand, secondOperand) {
+  return firstOperand * secondOperand;
+}
+function divide(firstOperand, secondOperand) {
+  return firstOperand / secondOperand;
+}
+
+console.log(subtract(35, 2));
+
+function operate(operator, firstOperand, secondOperand) {
+  return operator(firstOperand, secondOperand);
+}
+
+console.log(operate(add, 7, 3));
+
+function reset() {
+  firstOperand = "";
+  secondOperand = "";
+  operator = undefined;
+  lastOperationScreen.textContent = "";
+  currentOperationScreen.textContent = "";
+}
